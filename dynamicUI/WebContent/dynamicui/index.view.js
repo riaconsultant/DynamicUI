91ba7026d1bc0dd3c2a6e909a062eb47ac7c72f4
riaconsultant <manoj.chaurasiya@gmail.com>
2016-01-20 14:37:52 +0530

    First Commit

---
+++ dynamicUI/WebContent/dynamicui/index.view.js
@@ -0,0 +1,19 @@
+sap.ui.jsview("dynamicui.index", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf dynamicui.index
+	*/ 
+	getControllerName : function() {
+		return "dynamicui.index";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf dynamicui.index
+	*/ 
+	createContent : function(oController) {
+
+	}
+
+});
